c703e399acc4084814fb359d1dc54e329edfa85d
Raphiniel Murwira <raphinielr@gmail.com>
2023-11-16 01:35:33 +0200

    initial commit

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -7,6 +7,7 @@ const Top = () => {
         <h1 id="logo"><a href="">Raphiniel</a></h1>
         <div className="bottomTop">
           <p className="welcomeText">Welcome To My Website</p>
+          <p className="welcomeText">Still Under Construction</p>
           <div className="buttons">
           <button id="aboutBtn">About</button>
           <button id="emailMeBtn">Receive an Email</button>
